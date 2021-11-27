8f53cacdcb23db80d025523bacca9a674943c328
Danillo <danillodca@gmail.com>
2021-11-27 19:34:04 -0300

    primeiro commit

---
+++ 07-COBOL/ANTIGOS/PROGCOB01.cbl
@@ -0,0 +1,13 @@
+      ******************************************************************
+      *AUTHOR..: DANILLO
+      *OBJETIVO: MOSTRAR A STRING HELLO ALURA
+      *DATA....: 24/08/21
+      *
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROGCOB01.
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       PROCEDURE DIVISION.
+           DISPLAY 'HELLO ALURA'.
+           STOP RUN.
