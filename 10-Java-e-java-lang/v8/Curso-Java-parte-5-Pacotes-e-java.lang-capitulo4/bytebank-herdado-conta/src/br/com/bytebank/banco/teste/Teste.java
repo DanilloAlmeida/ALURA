799901b79957aeabf18aed99e40482d427fc13d3
Danillo <danillodca@gmail.com>
2021-12-30 05:32:34 -0300

    att 30-12-2021

--- 10-Java-e-java-lang/v8/Curso-Java-parte-5-Pacotes-e-java.lang-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
+++ 10-Java-e-java-lang/v8/Curso-Java-parte-5-Pacotes-e-java.lang-capitulo4/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/Teste.java
@@ -1,10 +1,17 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
+
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ContaPoupanca cp = new ContaPoupanca(2222, 22222);
+		ContaCorrente cc = new ContaCorrente(3333, 33333);
+		
+		
+		System.out.println(cc);
+		System.out.println(cp);
 	}
 
 }
