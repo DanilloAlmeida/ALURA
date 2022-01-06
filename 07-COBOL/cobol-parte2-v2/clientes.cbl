f9fdc6a2e0825b824998b4495f7e41861fad0ce8
Danillo <danillodca@gmail.com>
2022-01-06 06:14:12 -0300

    att cobol

--- 07-COBOL/cobol-parte2-v2/clientes.cbl
+++ 07-COBOL/cobol-parte2-v2/clientes.cbl
@@ -14,8 +14,10 @@
       *------------------------
        WORKING-STORAGE SECTION.
       *------------------------
-       77 WRK-OPCAO   PIC  X(01).
-       77 WRK-MODULO  PIC  X(25).
+       77 WRK-OPCAO            PIC   X(01).
+       77 WRK-MODULO           PIC   X(25).
+       77 WRK-OPCAO-RELATO     PIC   X(1).
+       77 WRK-TECLA            PIC   X(1).
 
       *------------------------
        SCREEN SECTION.
@@ -38,16 +40,24 @@
              05 LINE 10 COLUMN 15 VALUE '4 - EXCLUIR'.
              05 LINE 11 COLUMN 15 VALUE '5 - RELATORIO'.
              05 LINE 12 COLUMN 15 VALUE 'X - SAIDA'.
-             05 LINE 13 COLUMN 15 VALUE 'OPCAO..:'.
-             05 LINE 13 COLUMN 28 USING WRK-OPCAO.
+             05 LINE 13 COLUMN 15 VALUE 'OPCAO..: '.
+             05 LINE 13 COLUMN 24 USING WRK-OPCAO.
+
+       01 MENU-RELATORIO.
+             05 LINE 12 COLUMN 55 VALUE '1 - EM TELA'.
+             05 LINE 13 COLUMN 55 VALUE '2 - EM DISCO'.
+             05 LINE 14 COLUMN 55 VALUE 'OPCAO...: '.
+             05 LINE 14 COLUMN 65 USING WRK-OPCAO-RELATO.
 
       ******************************************************************
        PROCEDURE DIVISION.
       ******************************************************************
        0001-PRINCIPAL SECTION.
            PERFORM 1000-INICIAR.
-      *    PERFORM 2000-PRECESSAR.
+           PERFORM 2000-PROCESSAR.
       *    PERFORM 3000-FINALIZAR.
+
+           STOP RUN.
       *----------------------------------------------------------------*
        1000-INICIAR SECTION.
       *----------------------------------------------------------------*
@@ -57,7 +67,47 @@
        2000-PROCESSAR SECTION.
       *----------------------------------------------------------------*
 
-           MOVE 'MODULO - INCLUSAO' TO WRK-MODULO
+           EVALUATE WRK-OPCAO
+              WHEN 1
+      *           DISPLAY "VOCE INFORMOU 1"
+                 PERFORM 4000-INCLUIR
+      *           CONTINUE
+              WHEN 2
+                 DISPLAY "VOCE INFORMOU 2"
+                 CONTINUE
+              WHEN 3
+                 CONTINUE
+              WHEN 4
+                 CONTINUE
+              WHEN 5
+                 ACCEPT MENU-RELATORIO
+                 IF WRK-OPCAO-RELATO EQUAL 1
+                    PERFORM 5000-RELATORIO-TELA
+                 END-IF
+                 IF WRK-OPCAO-RELATO EQUAL 2
+                    PERFORM 5010-RELATORIO-DISCO
+                 END-IF
+              WHEN OTHER
+                 IF WRK-OPCAO NOT EQUAL 'X'
+                    DISPLAY 'ENTRADA INVALIDA!!!'
+                    ACCEPT WRK-TECLA
+                 END-IF
+              END-EVALUATE.
 
-           STOP RUN.
-       END PROGRAM CLIENTES.
+      *----------------------------------------------------------------*
+       4000-INCLUIR  SECTION.
+      *----------------------------------------------------------------*
+           MOVE 'NODULO - INCLUSAO' TO WRK-MODULO.
+           DISPLAY TELA.
+           ACCEPT WRK-TECLA AT 1620.
+      *     DISPLAY "VOCE INFORMOU 1"
+      *     CONTINUE.
+
+      *----------------------------------------------------------------*
+       5000-RELATORIO-TELA  SECTION.
+      *----------------------------------------------------------------*
+           CONTINUE.
+      *----------------------------------------------------------------*
+       5010-RELATORIO-DISCO  SECTION.
+      *----------------------------------------------------------------*
+           CONTINUE.
