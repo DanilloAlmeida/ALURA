d8105127202bffbf91c0c750fb222b60abf1842b
Danillo <danillodca@gmail.com>
2022-01-07 17:50:41 -0300

    foto

--- 13-javaScript/type-number.js
+++ 13-javaScript/type-number.js
@@ -7,3 +7,14 @@ const soma = primeiroNumero + segundoNumero
 const multiplicacao = primeiroNumero * segundoNumero
 console.log(soma)
 console.log(multiplicacao)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
+console.log(alura + primeiroNumero);
+//divisao por zero
+zeroA = 0;
+zeroB = 0;
+console.log ('0/0=' + zeroA/zeroB);
+zeroA = 5;
+zeroB = 0;
+console.log ('5/0=' + zeroA/zeroB)
