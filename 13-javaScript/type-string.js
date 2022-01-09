2a6ac781468bf852b91696bb4283f7c9741474c9
Danillo <danillodca@gmail.com>
2022-01-09 06:53:23 -0300

    primeiros exercicios

---
+++ 13-javaScript/type-string.js
@@ -0,0 +1,18 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+
+const citacao = 'O Léo disse "oi!"';
+console.log(citacao);
+
+//template string
+//template literal
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+tique= '\u2705'
+hiragana = '\u3041'
+
+console.log(cifrao)
+console.log(aMaiusculo)
+console.log(tique)
+console.log(hiragana)
