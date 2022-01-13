80cf7c936ea983592adf62d684befc97b9b13030
Danillo <danillodca@gmail.com>
2022-01-13 05:54:10 -0300

    att-13-01

---
+++ 13-javaScript/area.js
@@ -0,0 +1,18 @@
+/* var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+console.log(area);
+var area
+console.log(area); */
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+}
+
+console.log(area);
+//let area
--- 13-javaScript/type-bolean.js
+++ 13-javaScript/type-bolean.js
@@ -1,3 +1,12 @@
-const primeiroNumero = 5;
+/* const primeiroNumero = 5;
 const segundoNumero = 5;
 console.log(primeiroNumero === segundoNumero)
+
+a = 3
+b = "12"
+console.log(a+b);
+ */
+let minhaVar;
+let varNull = null;
+console.log(typeof minhaVar);
+console.log(typeof varNull);
