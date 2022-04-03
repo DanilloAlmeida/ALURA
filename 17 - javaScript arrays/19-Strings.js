ca20ac83286bc71aaa5565cfd75e9876323f73b4
Danillo <danillodca@gmail.com>
2022-04-03 08:37:31 -0300

    att

---
+++ 17 - javaScript arrays/19-Strings.js
@@ -0,0 +1,8 @@
+// const num = 500
+// console.log (num.toString())
+// console.log (num)
+
+let nome = "André"
+let comunicacao = "Olá, sr. nomeusuario, boa tarde!!!!"
+console.log(comunicacao.replace("nomeusuario", nome))
+console.log (comunicacao)
