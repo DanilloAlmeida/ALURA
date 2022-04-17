d4badb87ac3f2530d386b379113d5491c6d2a00f
Danillo <danillodca@gmail.com>
2022-04-17 08:20:04 -0300

    atualizando 17/04 8h19

--- 19-java-e-java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
+++ 19-java-e-java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TestArrayReferencias.java
@@ -6,17 +6,28 @@ public class TestArrayReferencias {
 
 	public static void main(String[] args) {
 	
-		ContaCorrente[] contas = new ContaCorrente[5];
+//		ContaCorrente[] contas = new ContaCorrente[5];
 		Conta[] contas = new Conta[5];
-		
-		
-		
 		
 		ContaCorrente cc1 = new ContaCorrente(2727, 001);
 		ContaCorrente cc2 = new ContaCorrente(2727, 002);
 		contas[0] = cc1;
 		contas[1] = cc2;
+		System.out.println("acessando diretamente");
+		System.out.println(contas[0].getAgencia());		
+		System.out.println(contas[1].getNumero());
 		
-		System.out.println(contas[0].getNumero());		
+		ContaCorrente ref = (ContaCorrente) contas[0];
+		System.out.println("\n\n acessando ref");
+		System.out.println(ref.getAgencia());		
+		System.out.println(ref.getNumero());		
+
+		ContaPoupanca cp1 = new ContaPoupanca(2727, 13001);
+		contas[2]= cp1;
+		
+		Conta ref2 = contas[2];
+		System.out.println("\n\n acessando ref");
+		System.out.println(ref2.getAgencia());
+		System.out.println(ref2.getNumero());
 	}
 }
---
+++ 19-java-e-java-util/bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Test.java
@@ -0,0 +1,13 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList lista = new Arrylis();
+		
+	}
+
+}
